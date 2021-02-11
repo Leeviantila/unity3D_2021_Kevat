53af2e59270d69df229e5d3151fc049620740e77
Lerppa20L04 <antila.leevi.04@gmail.com>
2021-02-11 18:22:45 +0200

    Pelihahmon luonti

---
+++ 2021_kevat_3Dpeli/Assets/Scripts/MouseLoop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLoop : MonoBehaviour
+{
+
+    public float mouseSnsitivity = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
