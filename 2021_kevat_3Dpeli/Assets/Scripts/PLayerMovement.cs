deda90434790217821b8267f9dba83a3b92d31da
Lerppa20L04 <antila.leevi.04@gmail.com>
2021-03-11 18:15:00 +0200

    Maassa olemisen tarkistus

--- 2021_kevat_3Dpeli/Assets/Scripts/PLayerMovement.cs
+++ 2021_kevat_3Dpeli/Assets/Scripts/PLayerMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PLayerMovement : MonoBehaviour
 {
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     public CharacterController controller;
     public float speed = 10f;
     public float gravity = -9.81f;
     private Vector3 velocity = Vector3.zero;
     public float jumpHeight = 3f;
+    private bool isGrounded;
+
 
 
 
@@ -27,17 +34,23 @@ public class PLayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        
+        
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
         
-        controller.Move(move * speed * Time.deltaTime);
         
-        if(Input.GetButtonDown("Jump")){
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            Jump();
 
         }
+
+        Move(x, z);
 
         velocity.y += gravity * Time.deltaTime;
 
@@ -47,5 +60,32 @@ public class PLayerMovement : MonoBehaviour
 
         
     }
+
+    public void Move(float x, float z){
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+    }
+
+
+    public void Jump(){
+        
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        
+        
+    }
+
+
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+
+
+
+    }
+
+
+
 }
  
