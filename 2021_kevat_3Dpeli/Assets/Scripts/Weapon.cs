0071bc4a138443c40a88179b73b96a7349e52210
Lerppa20L04 <antila.leevi.04@gmail.com>
2021-03-18 18:19:12 +0200

    Ampumisen lisääminen

---
+++ 2021_kevat_3Dpeli/Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float heittoVoima = 10f;
+    public GameObject heittoObjektiPrefab;
+    private Camera FPSCamera;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPSCamera = Camera.main;
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+
+            Heitto();
+        }
+
+    }
+
+    private void Heitto()
+    {
+        GameObject obj = Instantiate(heittoObjektiPrefab, transform.position, Quaternion.identity);
+        obj.GetComponent<Rigidbody>().AddForce(FPSCamera.transform.forward * heittoVoima);
+
+
+    }
+
+
+}
