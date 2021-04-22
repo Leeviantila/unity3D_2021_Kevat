9a97e443f547de9d623dab467d78aea1f63e8540
Lerppa20L04 <antila.leevi.04@gmail.com>
2021-04-22 18:13:10 +0300

    Journal -lisaaminen

---
+++ 2021_kevat_3Dpeli/Assets/Scripts/journal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class journal : MonoBehaviour
+{
+
+    public static journal Instance;
+
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            Instance = this;
+
+        }
+
+
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
